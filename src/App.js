501de6d8239885b9b345c89f403f94f51183139e
Quynh Le <107383112+QuynhLeSke@users.noreply.github.com>
2022-07-28 22:59:16 +0700

    Code quiz update test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+export function replaceCamelWithSpace(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
 
 function App() {
+  const [color, setColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
+  const newColor = color === "red" ? "blue" : "red";
+  const disaColor = disabled === false ? color : "gray";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className="btn"
+        style={{ backgroundColor: disaColor }}
+        onClick={() => setColor(newColor)}
+        disabled={disabled}
+      >
+        Change to {newColor}
+      </button>
+      <input
+        type="checkbox"
+        id="enable-btn-checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor="enable-btn-checkbox">Disable button</label>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,65 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App, { replaceCamelWithSpace } from "./App";
 
-test('renders learn react link', () => {
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole("button", {
+    name: "Change to blue",
+  });
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("button has correct color when clicked", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", {
+    name: "Change to blue",
+  });
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton.textContent).toBe("Change to red");
+});
+
+test("initail checkbox", () => {
+  render(<App />);
+  const colorButton = screen.getAllByRole("button", { name: "Change to blue" });
+  const checkbox = screen.getByRole("checkbox");
+  expect(colorButton).toBeEnabled;
+  expect(checkbox).not.toBeChecked;
+});
+
+test("disable and enable button by clicking checkbox", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled;
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeEnabled;
+});
+
+test("turn button gray on click", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle({ backgroundColor: "gray" });
+
+  fireEvent.click(checkbox);
+  expect(colorButton).not.toHaveStyle({ backgroundColor: "gray" });
+});
+
+describe("camel and space case", () => {
+  test("test camel with one word only", () => {
+    expect(replaceCamelWithSpace("Red")).toBe("Red");
+  });
+  test("test camel with 2 word", () => {
+    expect(replaceCamelWithSpace("VioletRed")).toBe("Violet Red");
+  });
+  test("test camel with one word only", () => {
+    expect(replaceCamelWithSpace("BlueRedWhite")).toBe("Blue Red White");
+  });
 });
